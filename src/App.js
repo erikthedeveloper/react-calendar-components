d4c04ea82aff78b099f0458dd8a60c84285e3522
Erik Aybar <erikthedeveloper@gmail.com>
2016-12-27 07:27:03 -0700

    Add barebones Calendar.{js,css}
    - Add src/components/ with Calendar.js component and Calendar.css styles
    - Render Calendar in App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Calendar from './components/Calendar';
 
 class App extends Component {
   render() {
@@ -8,11 +9,9 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>react-month-calendar</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Calendar />
       </div>
     );
   }
---
+++ src/components/Calendar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Calendar.css';
+
+class Calendar extends React.Component {
+  render() {
+    return (
+      <div className="Calendar">
+        Calendar Component
+      </div>
+    )
+  }
+}
+
+export default Calendar;
