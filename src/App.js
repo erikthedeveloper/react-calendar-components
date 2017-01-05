7ff010134ab858d1621c47210d1f377acee368c7
Erik Aybar <erikthedeveloper@gmail.com>
2017-01-05 10:58:01 -0700

    Remove Calendar from App (unbreak it)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Calendar from './components/Calendar';
 
 class App extends Component {
   render() {
@@ -11,7 +10,6 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>react-month-calendar</h2>
         </div>
-        <Calendar />
       </div>
     );
   }
