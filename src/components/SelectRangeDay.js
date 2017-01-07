6c9e4caff088ab9c4c45efc674716fdc395a6151
Erik Aybar <erikthedeveloper@gmail.com>
2017-01-07 06:36:44 -0700

    Add SelectRangeDay - HoC only

---
+++ src/components/SelectRangeDay.js
@@ -0,0 +1,44 @@
+import React, { PropTypes } from 'react';
+import { isInRange, isSameDay } from '../utils/date-utils';
+
+const rangeBarClassName = (date, range) => {
+  let className = 'Day__range-bar';
+  if (range.start && isSameDay(date, range.start)) {
+    className += ' Day__range-bar--start';
+  }
+  if (range.end && isSameDay(date, range.end)) {
+    className += ' Day__range-bar--end';
+  }
+
+  return className;
+};
+
+function rangeDay(Component) {
+
+  const RangeDay = (props) => {
+    const {start, end, hover} = props.range;
+
+    const displayRangeBar = isInRange(
+      props.date,
+      start,
+      end || hover || start
+    );
+
+    return (
+      <Component
+        onClick={props.onClick}
+        onMouseEnter={props.onMouseEnter}
+        {...props}
+      >
+        {props.children}
+        {displayRangeBar && (
+          <span className={rangeBarClassName(props.date, props.range)} />
+        )}
+      </Component>
+    );
+  };
+
+  return RangeDay;
+}
+
+export { rangeDay };
