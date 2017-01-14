7643bba6db52996ed65c1e64f7fb7cc3c5637839
Erik Aybar <erikthedeveloper@gmail.com>
2017-01-14 07:30:18 -0700

    Store enhancedDayComponent in state not on 'this'

--- src/components/Calendar/indicators.js
+++ src/components/Calendar/indicators.js
@@ -1,38 +1,59 @@
-import React, { PropTypes } from 'react';
+import React, { PropTypes, Component } from 'react';
 import { compose } from '../../utils/utils';
 import Day from '../Day/Day';
 import { indicatorDay } from '../Day/indicatorDay';
 import { isSameDay } from '../../utils/date-utils';
 
 /**
- * Higher Order Component to add "day indicators" feature
- * @param {Component} Component
+ * Higher Order Component to add "day indicators" feature to Calendar
+ * @param {Component} CalendarComponent
  * @return {IndicatorCalendar}
  */
-export function indicators(Component) {
-  class IndicatorCalendar extends React.Component {
+export function indicators(CalendarComponent) {
+  class IndicatorCalendar extends Component {
     constructor() {
       super(...arguments);
 
+      this.state = {
+        // Create enhancedDayComponent and store in state.
+        enhancedDayComponent: this.enhanceDayComponent(this.props.DayComponent),
+      };
+    }
+
+    componentDidUpdate(prevProps) {
+      // We don't want to create this on every render, but we also want
+      // to account for the fact that props.DayComponent could change
+      // We only want to re-create if props.DayComponent changed.
+      // Treat this as a "computed property".
+      if (prevProps.DayComponent !== this.props.DayComponent) {
+        const enhancedDayComponent = this.enhanceDayComponent(this.props.DayComponent);
+        this.setState({enhancedDayComponent});
+      }
+    }
+
+    /**
+     * @param {Component} DayComponent
+     * @return {Component} The enhanced DayComponent
+     */
+    enhanceDayComponent(DayComponent) {
       const hasIndicator = (date) => this.props.events.some(
         (event) => isSameDay(event.date, date)
       );
 
-      // The enhanced props.DayComponent
-      this.DayComponent = compose([
+      return compose([
         // 02 - Provide the enhanced DayComponent with
         // the required hasIndicator(Date):bool function
         (DayComponent) => (props) => (
           <DayComponent {...props} hasIndicator={hasIndicator} />
         ),
 
-        // 01 - Enhance the DayComponent
+        // 01 - Enhance the DayComponent to enable indicators
         indicatorDay,
-      ])(this.props.DayComponent);
+      ])(DayComponent);
     }
 
     render() {
-      return <Component {...this.props} DayComponent={this.DayComponent} />
+      return <CalendarComponent {...this.props} DayComponent={this.state.enhancedDayComponent} />
     }
   }
 
