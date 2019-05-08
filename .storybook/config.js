4d430fcc568a52dbd7c5faf30d3ca45b7a33461e
Erik Aybar <erikthedeveloper@gmail.com>
2019-05-08 06:48:34 -0600

    Add @storybook/addon-a11y
    https://github.com/storybooks/storybook/blob/next/addons/a11y/README.md

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,10 +1,13 @@
-import {configure, addParameters} from '@storybook/react';
+import {configure, addParameters, addDecorator} from '@storybook/react';
+import {withA11y} from '@storybook/addon-a11y';
 
 addParameters({
   options: {
     showPanel: false,
   },
 });
+
+addDecorator(withA11y);
 
 function loadStories() {
   require('../src/stories');
